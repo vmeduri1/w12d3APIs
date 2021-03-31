fd8a56b00685a75a0078606fb4f3d2455d48fb96
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-03-31 15:10:56 -0500

    Phase 2

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const morgan = require("morgan");
 const { environment } = require('./config');
 const app = express();
+const indexRouter = require('./routes/index');
+const tweetsRouter = require('./routes/tweets');
 
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the express-sequelize-starter!");
-});
+app.use(express.json());
+
+
 
 // Catch unhandled requests and forward to error handler.
 app.use((req, res, next) => {
