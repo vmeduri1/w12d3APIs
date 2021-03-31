54f6f29f870d1c738ff3a321a93d513592752e68
Christian Cozma <christiancozma.work@gmail.com>
2021-03-31 16:34:18 -0500

    phase errors I guess

--- routes/tweets.js
+++ routes/tweets.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require("../db/models");
 const { Tweet } = db;
+const { check, validationResult } = require('express-validator');
 
 const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next);
 
@@ -20,7 +21,6 @@ router.get('/tweets/:id(\\d+)', asyncHandler(async (req, res, next) => {
     } else {
         next(tweetNotFoundError(req.params.id));
     }
-
 }))
 
 const tweetNotFoundError = function (tweetId) {
@@ -29,9 +29,32 @@ const tweetNotFoundError = function (tweetId) {
     error.status = 404;
     return error;
 }
+const tweetValidators = [
+    check('message')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a message for Tweet')
+      .isLength({ max: 280 })
+      .withMessage('Tweet must not be more than 255 characters long')
+    ]
 
-router.post('/tweets', asyncHandler(async (req, res) => {
+router.post('/tweets', tweetValidators, asyncHandler(async (req, res) => {
+      const { message } = req.body
 
+      const tweet = db.Tweet.build({ message });
+
+      const validatorErrors = validationResult(req);
+
+      if(validatorErrors.isEmpty()) {
+          await tweet.save();
+          res.redirect('/');
+      } else {
+          const errors = validatorErrors.array().map(error => error.msg);
+          const err = Error("Bad request.");
+          err.errors = errors;
+          err.status = 400;
+          err.title = "Bad request.";
+          next(err);
+      }
 }))
 
 module.exports = router
