06bf78c3a8cfd9e751ae7851348092699235af88
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-04-01 16:48:54 -0500

    Where we got to

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,38 @@ const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
 const { asyncHandler, handleValidationErrors } = require("../utils");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+router.post("/", handleValidationErrors, asyncHandler(async (req, res) => {
+    const validateUsername =
+        check("username")
+            .exists({ checkFalsy: true })
+            .withMessage("Please provide a username");
 
+    const validateEmailAndPassword = [
+        check("email")
+            .exists({ checkFalsy: true })
+            .isEmail()
+            .withMessage("Please provide a valid email."),
+        check("password")
+            .exists({ checkFalsy: true })
+            .withMessage("Please provide a password."),
+    ];
 
-router.post("/")
+    router.post(
+        "/",
+        validateUsername,
+        validateEmailAndPassword,
+        handleValidationErrors,
+        asyncHandler(async (req, res) => {
+            const { username, email, password } = req.body;
+            //TODO: Create a user
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const user = await User.create( { username, email, hashedPassword });
+        })
+    )
+}))
 
 
 
